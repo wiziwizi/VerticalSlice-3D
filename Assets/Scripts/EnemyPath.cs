5585c6231e21ca8f65e48fe3c97c66d64ba12257
ninoosewoudt <nino.osewoudt@gmail.com>
2017-01-11 09:44:50 +0100

    enemy path

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour {
+	private bool toggle;
+	public WaypointsFollower wp;
+
+	void Start()
+	{
+			StartCoroutine(SpawnWaypoint());
+	}
+
+	IEnumerator SpawnWaypoint() 
+	{
+		while(true)
+		{
+		wp.AddWayPoint(transform.position);
+		yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
