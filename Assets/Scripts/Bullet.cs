3f5f9f20cf52b1a9073205ac1709dff779527dc5
ninoosewoudt <nino.osewoudt@gmail.com>
2017-01-26 11:09:09 +0100

    minimap

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public float BulletSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Destroy(gameObject , 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * BulletSpeed * Time.deltaTime);
+		
+	}
+}
